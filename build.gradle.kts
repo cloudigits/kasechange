49f39b9af40bd12dfa95b1169ce4179dcc7099af
azizatoev <a.atoev93@gmail.com>
2026-07-18 06:25:38 +0500

    Replace net.pearx.multigradle with plain KMP plugin, add wasmJs target
    Bump Gradle 7.6.1->9.6.1 and Kotlin 1.8.20->2.4.0; multigradle 1.13.0
    predates hierarchical source sets and stopped declaring any targets
    under current Kotlin. Source is pure commonMain string manipulation
    with no platform APIs, so no source-set restructuring was needed.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,117 +1,51 @@
-import com.github.breadmoirai.githubreleaseplugin.GithubReleaseExtension
-import net.pearx.multigradle.util.MultiGradleExtension
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
-val projectChangelog: String by project
 val projectDescription: String by project
-
-val sonatypeOssUsername: String? by project
-val sonatypeOssPassword: String? by project
-val githubAccessToken: String? by project
-val devBuildNumber: String? by project
-
+val projectVersion: String by project
 
 description = projectDescription
-
+group = "net.pearx.kasechange"
+version = projectVersion
 
 plugins {
-    id("net.pearx.multigradle.simple.project")
-    id("org.jetbrains.kotlin.multiplatform") apply (false)
-    id("com.github.breadmoirai.github-release")
+    kotlin("multiplatform")
     `maven-publish`
-    signing
 }
 
-configure<MultiGradleExtension> {
-    if (devBuildNumber != null) {
-        projectVersion = "$projectVersion-dev-$devBuildNumber"
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm()
+
+    js {
+        browser()
+        nodejs()
     }
-}
 
-configure<PublishingExtension> {
-    publications.withType<MavenPublication> {
-        pom {
-            name.set(artifactId)
-            description.set(projectDescription)
-            url.set("https://github.com/pearxteam/kasechange")
-            licenses {
-                license {
-                    name.set("Mozilla Public License, Version 2.0")
-                    url.set("https://mozilla.org/MPL/2.0/")
-                    distribution.set("repo")
-                }
-            }
-            organization {
-                name.set("PearX Team")
-                url.set("https://pearx.net/")
-            }
-            developers {
-                developer {
-                    id.set("mrAppleXZ")
-                    name.set("mrAppleXZ")
-                    email.set("me@pearx.net")
-                    url.set("https://pearx.net/members/mrapplexz")
-                    organization.set("PearX Team")
-                    organizationUrl.set("https://pearx.net/")
-                    roles.set(listOf("developer"))
-                    timezone.set("Asia/Yekaterinburg")
-                }
-            }
-            scm {
-                url.set("https://github.com/pearxteam/kasechange")
-                connection.set("scm:git:git://github.com/pearxteam/kasechange")
-                developerConnection.set("scm:git:git://github.com/pearxteam/kasechange")
-            }
-            issueManagement {
-                system.set("GitHub")
-                url.set("https://github.com/pearxteam/kasechange/issues")
-            }
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs { browser() }
+
+    iosArm64()
+    iosX64()
+    iosSimulatorArm64()
+
+    macosX64()
+    macosArm64()
+
+    linuxX64()
+    mingwX64()
+
+    sourceSets {
+        commonTest.dependencies {
+            implementation(kotlin("test"))
         }
     }
+}
+
+publishing {
     repositories {
-        maven {
-            credentials {
-                username = "pearxteam"
-                password = githubAccessToken
-            }
-            name = "github-all"
-            url = uri("https://maven.pkg.github.com/pearxteam/kasechange")
-        }
-        maven {
-            credentials {
-                username = sonatypeOssUsername
-                password = sonatypeOssPassword
-            }
-            name = "sonatype-oss-release"
-            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-        }
+        mavenLocal()
     }
-}
-
-tasks {
-    val publishDevelop by registering {
-        group = "publishing"
-        dependsOn(withType<PublishToMavenRepository>().matching { it.repository.name.endsWith("-develop") or it.repository.name.endsWith("-all") })
-    }
-    val publishRelease by registering {
-        group = "publishing"
-        dependsOn(withType<PublishToMavenRepository>().matching { it.repository.name.endsWith("-release") or it.repository.name.endsWith("-all") })
-    }
-    val release by registering {
-        group = "publishing"
-        dependsOn(publishRelease)
-        dependsOn(named("githubRelease"))
-    }
-}
-
-configure<SigningExtension> {
-    sign(publishing.publications)
-}
-
-configure<GithubReleaseExtension> {
-    setToken(githubAccessToken)
-    setOwner("pearxteam")
-    setRepo("kasechange")
-    setTargetCommitish("master")
-    setBody(projectChangelog)
-    //setReleaseAssets((publishing.publications["maven"] as MavenPublication).artifacts.map { it.file })
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,14 @@
 rootProject.name = "kasechange"
 
 pluginManagement {
-    val multigradleVersion: String by settings
     val kotlinVersion: String by settings
-    val githubReleaseVersion: String by settings
+
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
 
     plugins {
         id("org.jetbrains.kotlin.multiplatform") version kotlinVersion
-        id("net.pearx.multigradle.simple.project") version multigradleVersion
-        id("net.pearx.multigradle.simple.settings") version multigradleVersion
-        id("com.github.breadmoirai.github-release") version githubReleaseVersion
     }
-}
-
-plugins {
-    id("net.pearx.multigradle.simple.settings")
 }
